106e4ee20f93008ef5a5ab75982928b2d7cb2621
Janek Spaderna <janek@spaderna.eu>
2024-09-17 16:07:13 +0200

    We can print functions

--- c/asm.c
+++ c/asm.c
@@ -3,9 +3,12 @@
 #include "dom.h"
 
 #include <errno.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/_types/_va_list.h>
+#include <sys/cdefs.h>
 #include <unistd.h>
 
 #define RT_DUMMY                                                               \
@@ -73,10 +76,11 @@ int read_file(FILE *in, str *s) {
 #define INBOUNDS(a, n) (0 <= n && (size_t)n < (sizeof a / sizeof *a))
 
 #define C_RST ""         // reset
-#define C_OPC "1;7"      // bold
+#define C_OPC "1;7"      // bold, reverse
 #define C_ARG "38;5;251" // white
 #define C_LIT "32"       // green
 #define C_OPN "35"       // magenta
+#define C_ERR "1;31"     // bold, red
 
 static int DO_COLORS;
 #define C(name) (DO_COLORS ? "\033[" C_##name "m" : "")
@@ -175,6 +179,69 @@ int fn_refs_insert(fn_refs *refs, lv *fn) {
   return 1;
 }
 
+__dead2 __printflike(1, 2) void err_exit(const char *fmt, ...) {
+  fprintf(stderr, "%serror:%s ", C(ERR), C(RST));
+  va_list ap;
+  va_start(ap, fmt);
+  vfprintf(stderr, fmt, ap);
+  va_end(ap);
+  putc('\n', stderr);
+  exit(1);
+}
+
+void pretty_block(lv *b, fn_refs *referenced_fns) {
+  const char *const opnames[] = {
+      "JUMP", "JUMPF", "LIT",  "DUP", "DROP", "SWAP",  "OVER", "BUND", "OP1",
+      "OP2",  "OP3",   "GET",  "SET", "LOC",  "AMEND", "TAIL", "CALL", "BIND",
+      "ITER", "EACH",  "NEXT", "COL", "IPRE", "IPOST", "FIDX", "FMAP",
+  };
+  const int opcount = (int)(sizeof opnames / sizeof *opnames);
+
+  for (int width, pc = 0; pc < b->n; pc += width) {
+    // Sanity check that it is valid to index `opnames` & `oplens`.
+    int op = b->sv[pc];
+    if (op < 0 || opcount <= op) {
+      err_exit("invalid opcode byte %x at offset %d\n", op, pc);
+    }
+
+    // Sanity check that we will not read outside the program buffer when
+    // decoding this instruction.
+    width = oplens[op];
+    int missing = pc + width - b->n;
+    if (missing > 0) {
+      err_exit("bad opcode: %s missing %d byte%s\n", opnames[op], missing,
+               missing == 1 ? "" : "s");
+    }
+
+    int imm = width == 3 ? blk_gets(b, pc + 1) : -1;
+    if (op == LIT) {
+      lv *lit = blk_getimm(b, imm);
+      if (lion(lit)) {
+        fn_refs_insert(referenced_fns, lit);
+      }
+    }
+
+    int fmtlen = 0;
+    printf("%s", C(OPC));
+    fmtlen += printf("[%0.4x] %s", pc, opnames[op]);
+    if (imm != -1) {
+      const char *pretty = fmt_imm(op, b, imm, &fmtlen);
+      fmtlen += printf(" (#%d)", imm);
+      // printf("%s", C(RST));
+      printf("%s%s", pretty ? "  " : "", pretty ? pretty : "");
+      fmtlen += pretty ? 2 : 0;
+    }
+    if (fmtlen < 80 && DO_COLORS)
+      printf("%s%*s", C(OPC), 80 - fmtlen, "");
+    printf("%s\n", C(RST));
+
+    for (int i = 1; i < width; ++i) {
+      printf("%s[%0.4x]   0x%0.2x%s\n", C(ARG), pc + i, (int)b->sv[pc + i],
+             C(RST));
+    }
+  }
+}
+
 int main(int argc, char **argv) {
   DO_COLORS = isatty(STDOUT_FILENO);
   EXE = argc ? argv[0] : "<exe>";
@@ -239,68 +306,16 @@ int main(int argc, char **argv) {
 
   lv *prog = parse(expr ? expr : src_str.sv);
   if (perr()) {
-    fprintf(stderr, "error:%s:%d:%d: %s\n", argc ? argv[0] : "stdin", par.r + 1,
-            par.c + 1, par.error);
-    return 1;
+    err_exit("%s:%d:%d: %s", argc ? argv[0] : "«stdin»", par.r + 1, par.c + 1,
+             par.error);
   }
-
-  static const char *const opnames[] = {
-      "JUMP", "JUMPF", "LIT",  "DUP", "DROP", "SWAP",  "OVER", "BUND", "OP1",
-      "OP2",  "OP3",   "GET",  "SET", "LOC",  "AMEND", "TAIL", "CALL", "BIND",
-      "ITER", "EACH",  "NEXT", "COL", "IPRE", "IPOST", "FIDX", "FMAP",
-  };
-  const int opcount = (int)(sizeof opnames / sizeof *opnames);
 
   fn_refs referenced = {0};
+  pretty_block(prog, &referenced);
 
-  for (int width, pc = 0; pc < prog->n; pc += width) {
-    // Sanity check that it is valid to index `opnames` & `oplens`.
-    int op = prog->sv[pc];
-    if (op < 0 || opcount <= op) {
-      fprintf(stderr, "invalid opcode byte %x at offset %d\n", op, pc);
-      return 1;
-    }
-
-    // Sanity check that we will not read outside the program buffer when
-    // decoding this instruction.
-    width = oplens[op];
-    int missing = pc + width - prog->n;
-    if (missing > 0) {
-      fprintf(stderr, "bad opcode: %s missing %d byte%s\n", opnames[op],
-              missing, missing == 1 ? "" : "s");
-      return 1;
-    }
-
-    int imm = width == 3 ? blk_gets(prog, pc + 1) : -1;
-    if (op == LIT) {
-      lv *lit = blk_getimm(prog, imm);
-      if (lion(lit)) {
-        fn_refs_insert(&referenced, lit);
-      }
-    }
-
-    int fmtlen = 0;
-    printf("%s", C(OPC));
-    fmtlen += printf("[%0.4x] %s", pc, opnames[op]);
-    if (imm != -1) {
-      const char *pretty = fmt_imm(op, prog, imm, &fmtlen);
-      fmtlen += printf(" (#%d)", imm);
-      // printf("%s", C(RST));
-      printf("%s%s", pretty ? "  " : "", pretty ? pretty : "");
-      fmtlen += pretty ? 2 : 0;
-    }
-    if (fmtlen < 80 && DO_COLORS)
-      printf("%s%*s", C(OPC), 80 - fmtlen, "");
-    printf("%s\n", C(RST));
-
-    for (int i = 1; i < width; ++i) {
-      printf("%s[%0.4x]   0x%0.2x%s\n", C(ARG), pc + i, (int)prog->sv[pc + i],
-             C(RST));
-    }
-  }
-
-  printf("\nREFERENCED FUNCTIONS: %zu\n", referenced.size);
-  for (size_t i = 0; i < referenced.size; ++i) {
-    printf("  - %s\n", referenced.fns[i]->sv);
+  for (size_t ref_idx = 0; ref_idx < referenced.size; ++ref_idx) {
+    lv *fn = referenced.fns[ref_idx];
+    printf("\n«%s»\n", fn->sv);
+    pretty_block(fn->b, &referenced);
   }
 }
